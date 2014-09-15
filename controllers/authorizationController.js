ee4917c745c50d777f7abc00d2698695291afc98
raposinho <a33724@alunos.isel.pt>
2014-09-15 22:37:41 +0100

    modificada novamente a frase de sucesso

--- controllers/authorizationController.js
+++ controllers/authorizationController.js
@@ -109,7 +109,7 @@ function startTokenRequest(req, res) {
         var items = responseFormatter.commonAuthorizationFields(req,
                                                                 'Authorization success',
                                                                 reqInfo.targetService,
-                                                                'was successful, application is finishing preparations');
+                                                                'was successful, application is wrapping up operations. You may close this window');
         endResponses.html(200, items, res, 'authorization/commonAuthorization');
     }
 }
