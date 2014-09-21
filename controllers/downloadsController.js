5e2611c9e316539204598ab0d342bff32de5c5cf
raposinho <a33724@alunos.isel.pt>
2014-09-21 21:34:59 +0100

    modificao 3

--- controllers/downloadsController.js
+++ controllers/downloadsController.js
@@ -2,8 +2,7 @@ var fs = require('fs'),
     path = require('path');
 
 module.exports = {
-    startDownload: startDownload,
-    startDownload2: startDownload2
+    startDownload: startDownload
 }
 
 function startDownload(req, res) {
@@ -12,12 +11,4 @@ function startDownload(req, res) {
 
     var appDir = path.dirname(require.main.filename);
     fs.createReadStream(appDir + '/public/files/CloudomaticClient.zip').pipe(res);
-}
-
-function startDownload2(req, res) {
-    res.setHeader('Content-disposition', 'attachment; filename=CloudomaticClient2.png');
-    res.setHeader('Content-type', 'image/png');
-
-    var appDir = path.dirname(require.main.filename);
-    fs.createReadStream(appDir + '/public/files/CloudomaticClient2.png').pipe(res);
 }
